a8eb0780f17bad5632dcbbb5d016758ad04e076d
Stefan Lier <lier.stefan@gmail.com>
2024-09-08 18:30:48 +0200

    feat: read experiences from static json

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,12 @@ import headshot from './images/headshot.png'
 import {IconBrandGithub, IconBrandLinkedin, IconMail} from '@tabler/icons-react';
 import ExperienceCard from "@/app/components/ExperienceCard/ExperienceCard";
 import Link from "next/link";
+import experienceData from "./data/experience.json";
 
 export default function Home() {
+
     return (
-        <div className={'max-w-xl p-2 space-y-2 mt-10'}>
+        <div className={'max-w-xl p-2 space-y-2 m-10'}>
             <header className={'flex flex-col items-center space-y-2 mb-5'}>
                 <h1>Stefan Lier</h1>
                 <Image
@@ -37,39 +39,15 @@ export default function Home() {
                     <header>
                         <h2 className={'mt-5'}>What I&apos;ve been up to lately</h2>
                     </header>
-                    <main>
-                        <ExperienceCard
-                            title="Architect"
-                            timeframe="Feb 2024 - Aug 2024"
-                            description="Being embedded in a team of three architects, I was responsible for aligning the
-                        Macro-Architecture with the teams and collect both business and technical needs for it. This
-                        helped to define guidelines for the teams to follow and implement their services against,
-                        creating a unified vision for all products. To effectively steer those efforts, we were creating
-                        formats to share, discuss and align team-needs and help them to take ownership of their own
-                        implementation-details."
-                        />
-                        <ExperienceCard
-                            title="Product Architect"
-                            timeframe="Jan 2023 - Jan 2024"
-                            description="
-                            As a Product Architect, Stefan provided guidance for five application teams, aligning
-                            product architectures with the macro-architecture. He facilitated product-related and
-                            technical formats to understand needs and coordinate implementation efforts. Being part of
-                            the teams allowed him to maintain proximity and ensure alignment between micro and macro
-                            architectures. Stefans approach facilitated swift decision-making and effective development
-                            coordination, fostering a culture of knowledge sharing across teams."
-                        />
-                        <ExperienceCard
-                            title="Team and Tech Lead"
-                            timeframe="Jul 2022 - Dec 2022"
-                            description="
-                            Leading a cloud migration project, Stefan orchestrated the technical roadmap and
-                            communication with stakeholders. He crafted a modern, event-driven architecture leveraging
-                            both on-premise and Google Cloud capabilities. Through Infrastructure-as-Code, he ensured
-                            repeatability and accountability in infrastructure changes. Stefans leadership fostered a
-                            culture of knowledge sharing, driving community practices and supporting team onboarding in
-                            new technologies."
-                        />
+                    <main className={'flex flex-col gap-2'}>
+                        {experienceData.map((experience, index) => (
+                            <ExperienceCard
+                                key={index}
+                                title={experience.title}
+                                timeframe={experience.timeframe}
+                                description={experience.description}
+                            />
+                        ))}
                     </main>
                 </section>
             </main>
