410225290283ba8f1b18eae6cf32f6ae024703d5
Stefan Lier <lier.stefan@gmail.com>
2024-09-08 20:15:29 +0200

    feat: adjust the presentation of experiences

--- app/components/ExperienceCard/ExperienceCard.tsx
+++ app/components/ExperienceCard/ExperienceCard.tsx
@@ -7,9 +7,9 @@ interface ExperienceProperties {
 function ExperienceCard(props: ExperienceProperties) {
     return (
         <div className={'space-y-1 rounded-xl border-black bg-offwhite border p-2  drop-shadow-md'}>
-            <header className={'flex flex-row gap-2'}>
-                <h3>{props.title}</h3>
-                <span>{props.timeframe}</span>
+            <header className={'flex flex-col mb-2'}>
+                <h3 className={'font-bold text-accent'}>{props.title}</h3>
+                <div className={'text-md font-extralight'}>{props.timeframe}</div>
             </header>
             <main>
                 {props.description}
