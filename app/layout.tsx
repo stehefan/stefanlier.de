97d740b150458ea19b7908a333aa8bebd2ac94ff
Stefan Lier <lier.stefan@gmail.com>
2024-12-01 11:44:29 +0100

    fix: set ssr to false for ph component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,16 @@
 import type { Metadata } from 'next';
 import { PHProvider } from './providers';
 import './globals.css';
-import PostHogPageView from '@/app/PostHogPageView';
+import dynamic from 'next/dynamic';
 
 export const metadata: Metadata = {
     title: 'Home of Stefan Lier',
     description: 'Personal Website of Stefan Lier',
 };
+
+const PostHogPageView = dynamic(() => import('./PostHogPageView'), {
+    ssr: false,
+});
 
 export default function RootLayout({
     children,
