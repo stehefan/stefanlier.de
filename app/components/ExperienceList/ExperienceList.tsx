8c994c4a69030878c8b92e07776b84a47714b046
Stefan Lier <lier.stefan@gmail.com>
2024-12-25 21:05:27 +0100

    feat: remove posthog- and count-related code as it's not used anymore

--- app/components/ExperienceList/ExperienceList.tsx
+++ app/components/ExperienceList/ExperienceList.tsx
@@ -1,17 +1,12 @@
 'use client';
 
 import ExperienceCard from '@/app/components/ExperienceCard/ExperienceCard';
-import { useState } from 'react';
-import { usePostHog } from 'posthog-js/react';
 
 interface ExperienceListProperties {
     data: Experience[];
 }
 
 function ExperienceList(props: ExperienceListProperties) {
-    const [numberOfExperiences, setNumberOfExperiences] = useState(3);
-    const posthog = usePostHog();
-
     return (
         <div className="flex flex-col w-full space-y-3">
             {props.data.map((experience, index) => (
